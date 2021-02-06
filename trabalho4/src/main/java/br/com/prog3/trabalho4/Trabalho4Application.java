7596a2a0e26b9cef340927e82f341b4625aa39e7
IIcobus2 <49793357+IIcobus2@users.noreply.github.com>
2021-02-06 17:53:23 -0300

    trabalho finalizado

--- trabalho4/src/main/java/br/com/prog3/trabalho4/Trabalho4Application.java
+++ trabalho4/src/main/java/br/com/prog3/trabalho4/Trabalho4Application.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package br.com.prog3.trabalho4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
