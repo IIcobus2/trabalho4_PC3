d0aee39045989bab583d55302dfa3a60ecba8414
IIcobus2 <49793357+IIcobus2@users.noreply.github.com>
2021-02-06 15:32:04 -0300

    Spring Initializr

---
+++ demo/src/main/java/com/example/demo/Trabalho4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho4Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/example/demo/Trabalho4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
